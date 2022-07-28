1571507feda1b871488314330706f96db773ce8d
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-07-28 13:17:47 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,12 @@
+class ArithmeticOperator
+{
+	public static void main(String args[])
+	{
+		int num1=18;
+		int num2=7;
+		System.out.println("Addition ="+(num1+num2));
+		System.out.println("Substraction ="+(num1-num2));
+		System.out.println("Multiplication ="+(num1*num2));
+		System.out.println("division ="+(num1/num2));
+	}
+}
