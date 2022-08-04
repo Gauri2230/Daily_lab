81cf5ec6b7fb9d07ef2c73d64ca653d476b40e15
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-08-04 11:01:25 +0530

    Add files via upload

---
+++ StringBufferEx.java
@@ -0,0 +1,9 @@
+class StringBufferEx
+{  
+	public static void main(String args[])
+	{  
+		StringBuffer sb=new StringBuffer("Hello ");  
+		sb.append("Anudip");
+		System.out.println(sb); 
+	}  
+}  
